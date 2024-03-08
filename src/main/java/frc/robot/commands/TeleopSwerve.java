acf1af6a41887427453ec02329be245e4c4e180b
Rayirth <rayirthd@gmail.com>
2024-03-08 00:23:36 -0800

    swerve encoder fixed?

--- src/main/java/frc/robot/commands/TeleopSwerve.java
+++ src/main/java/frc/robot/commands/TeleopSwerve.java
@@ -5,54 +5,78 @@ import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj2.command.Command;
+
 import frc.robot.Constants;
 import frc.robot.RobotContainer;
 
+// IMPORTS THAT ARE NOT CURRENTLY USED
+// import frc.robot.Robot;
+// import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+// import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+// import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
+// import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
+// import edu.wpi.first.math.geometry.Pose2d;
+
 public class TeleopSwerve extends Command {
 
+  // Flag used to prevent spin up at the start
+  boolean initFlag = true;
+
+  // flag used for sync
+  double encoderOffset;
+
+  // Double suppliers for the movement values for swerve
   static private DoubleSupplier translationSup;
   static private DoubleSupplier strafeSup;
   static private DoubleSupplier rotationSup;
   static private BooleanSupplier robotCentricSup;
 
+  // Encoder/rotational pid values, constraints, and controller
   static double encoderkP = 0.08;
   static double encoderkI = 0.00;
   static double encoderkD = 0.005;
+  static double ff = 0.005;
+  TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(1000, 100);
+  ProfiledPIDController rotPid = new ProfiledPIDController(encoderkP, encoderkI, encoderkD, constraints);
 
- static double ff = 0.005;
-
-  
- 
-
+  // Shuffleboard entries
   static GenericEntry pEntry = Shuffleboard.getTab("PID").add("Proportional", encoderkP).getEntry();
   static GenericEntry iEntry = Shuffleboard.getTab("PID").add("Integral",encoderkI).getEntry();
   static GenericEntry dEntry = Shuffleboard.getTab("PID").add("Derivative",encoderkD).getEntry();
   static GenericEntry ffEntry = Shuffleboard.getTab("PID").add("ff",ff).getEntry();
   static GenericEntry rotValEntry = Shuffleboard.getTab("PID").add("outVal",0).getEntry();
   static GenericEntry diffValEntry = Shuffleboard.getTab("PID").add("diffVal",0).getEntry();
-  //static GenericEntry ffEntry = Shuffleboard.getTab("PID").add("Feed Forward",encoderkD).getEntry();
+  static GenericEntry betterEncoderEntry = Shuffleboard.getTab("Swerve").add("Encoder Target Angle",0).getEntry();
+  // static GenericEntry ffEntry = Shuffleboard.getTab("PID").add("Feed Forward",encoderkD).getEntry();
   // static GenericEntry velEntry = Shuffleboard.getTab("PID").add("VelTOl",encoderkD).getEntry();
-  // static GenericEntry posEntry = Shuffleboard.getTab("PID").add("PostTOl",encoderkD).getEntry();
- 
+  // static GenericEntry posEntry = Shuffleboard.getTab("PID").add("PostTOl",encoderkD).getEntry();  
 
-  TrapezoidProfile.Constraints constraints = new TrapezoidProfile.Constraints(100, 100);
-  ProfiledPIDController rotational_pid = new ProfiledPIDController(encoderkP, encoderkI, encoderkD, constraints);
-  static double betterEncoderAngle;
+  // The variable that holds the target angle for swerve to rotate to
+
+  static double betterEncoderAngle = 0;//(RobotContainer.operatorBoard.getRawAxis(3) + 1) * 180;
+
+
+  // Default swerve command
 
   public TeleopSwerve() {
     addRequirements(RobotContainer.s_Swerve);
-    rotational_pid.enableContinuousInput(0, 360);
+    rotPid.enableContinuousInput(0, 360);
   }
+
+  // Logarithmic control for swerve so that small movements remain small and large movements are large
 
   private static double logAxis(double value) {
     value = Math.copySign(Math.log((Math.abs(value) + 1)) / Math.log(2), value);
     return value;
   }
+
+  // Squares for the same reason as the logarithm earlier, just for better control
 
   private static double squareAxis(double value, double deadband) {
     value = MathUtil.applyDeadband(value, deadband);
@@ -60,15 +84,23 @@ public class TeleopSwerve extends Command {
     return value;
   }
 
+  public double getBetterEncoderAngle() {
+    return betterEncoderAngle;
+  }
+
   @Override
   public void execute() {
 
-   rotational_pid.setP(pEntry.getDouble(0));
-   rotational_pid.setI(iEntry.getDouble(0));
-   rotational_pid.setD(dEntry.getDouble(0));
+    // Shuffleboard entries for rotational PID
 
-   
-  //rotational_pid.setTolerance(posEntry.getDouble(0), velEntry.getDouble(0));
+    rotPid.setP(pEntry.getDouble(0));
+    rotPid.setI(iEntry.getDouble(0));
+    rotPid.setD(dEntry.getDouble(0));
+    betterEncoderEntry.setDouble(betterEncoderAngle);
+
+    // Shuffleboard entries for encoder
+
+    // Switches modes between joystick/encoder rotational control TODO: change this to swap between flight controller and xbox
 
     if (!RobotContainer.turnSwitch.getAsBoolean()) {
       rotationSup = () -> RobotContainer.operatorBoard.getRawAxis(3);
@@ -76,21 +108,56 @@ public class TeleopSwerve extends Command {
       rotationSup = () -> RobotContainer.driver.getRawAxis(2);
     }
 
+    // Initializes movement suppliers to controller axis
+
     translationSup = () -> -RobotContainer.driver.getRawAxis(1);
     strafeSup = () -> -RobotContainer.driver.getRawAxis(0);
     robotCentricSup = () -> RobotContainer.robotCentric.getAsBoolean();
 
-    betterEncoderAngle = (rotationSup.getAsDouble() + 1) * 180;
-    /*
+    // Attempts to zero the robot at startup and wait for a new angle to go to
+
+    if (initFlag) {
+      RobotContainer.s_Swerve.zeroHeading();
+      encoderOffset = RobotContainer.driver.getRawAxis(2);
+      initFlag = false;
+    }
+    else if (rotationSup.getAsDouble() + 1 - encoderOffset < 0) {
+      System.out.println("im below 0");
+      betterEncoderAngle = (2 + rotationSup.getAsDouble() + 1 - encoderOffset) * 180;
+    }
+    else if (rotationSup.getAsDouble() + 1 - encoderOffset > 2) {
+      System.out.println("im above 2");
+      betterEncoderAngle = (rotationSup.getAsDouble() + 1 - encoderOffset - 2) * 180;
+    }
+    else
+      betterEncoderAngle = (rotationSup.getAsDouble() + 1 - encoderOffset) * 180;
+    //System.out.println(betterEncoderAngle);
+    // else {
+    //   betterEncoderAngle = (rotationSup.getAsDouble() + 1) * 180;
+    // }
+    
+    // Not sure what this is, leftover testing statements
+
+      /*
      * betterEncoderAngle = ((rotationSup.getAsDouble() + 1) * )%360
      */
     // System.out.println("Better encoder angle: " + betterEncoderAngle);
     // System.out.println("RotationSup: " + rotationSup.getAsDouble());
+    
+    
+    // Convert double supplier to double
+
     double translationVal = translationSup.getAsDouble();
     double strafeVal = strafeSup.getAsDouble();
     double rotationVal = rotationSup.getAsDouble();
+
+    // Calculate the difference between the encoder angle and the gyro angle
+
     double new_Var = ((RobotContainer.s_Swerve.getGyroYaw().getDegrees()) - betterEncoderAngle);
     
+    // Optimization; for example, if the robot is commanded to spin 190 degrees, it will instead spin 170 degrees in the other direction.
+    // can cause bugs so comment out while testing
+
     // if(new_Var > 180){
     //   new_Var -= 180;
     // }
@@ -98,21 +165,27 @@ public class TeleopSwerve extends Command {
     //   new_Var += 180;
     // }
 
+    // Applying deadband and smoother control
+
     translationVal = squareAxis(logAxis(translationVal), Constants.stickDeadband);
     strafeVal = squareAxis(logAxis(strafeVal), Constants.stickDeadband);
 
-    if (!RobotContainer.turnSwitch.getAsBoolean() && RobotContainer.operatorBoard.isConnected()) {
+    // Check what control to use based on switch (whether joystick or encoder rotation)
 
-      rotationVal = rotational_pid.calculate((RobotContainer.s_Swerve.getGyroYaw().getDegrees()) , betterEncoderAngle ) ; // m_feedforward.calculate(rotational_pid.getSetpoint().velocity, rotationVal, rotationVal, rotationVal, rotationVal);
-      rotationVal += (-ffEntry.getDouble(ff) * new_Var);
-      
-    } else {
-      rotationVal = squareAxis(logAxis(rotationVal), Constants.stickRotationDeadband)
-          * Constants.Swerve.maxAngularVelocity / 4;
+    if (!RobotContainer.turnSwitch.getAsBoolean() && RobotContainer.operatorBoard.isConnected()) {
+      rotationVal = rotPid.calculate((RobotContainer.s_Swerve.getGyroYaw().getDegrees()), betterEncoderAngle); // m_feedforward.calculate(rotPid.getSetpoint().velocity, rotationVal, rotationVal, rotationVal, rotationVal);
+      rotationVal += (-ffEntry.getDouble(ff) /* new_Var*/);  
     }
+    else {
+      rotationVal = squareAxis(logAxis(rotationVal), Constants.stickRotationDeadband) * Constants.Swerve.maxAngularVelocity / 4;
+    }
+
+    // Updating shuffleboard entries for rotation values
 
     rotValEntry.setDouble(rotationVal);
     diffValEntry.setDouble(new_Var);
+
+    // Calling drive method in subsystem to move robot
 
     RobotContainer.s_Swerve.drive(
         new Translation2d(translationVal, strafeVal).times(Constants.Swerve.maxSpeed),
--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -1,10 +1,12 @@
 package frc.robot.subsystems;
 
+import java.util.Map;
 import java.util.Optional;
 
 import com.ctre.phoenix6.configs.Pigeon2Configuration;
 import com.ctre.phoenix6.hardware.Pigeon2;
 
+import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
@@ -14,9 +16,14 @@ import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.networktables.GenericEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructArrayPublisher;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -30,7 +37,19 @@ public class Swerve extends SubsystemBase {
 
     public SwerveDriveOdometry swerveOdometry;
     public SwerveModule[] mSwerveMods;
-    public Pigeon2 gyro;
+    public static Pigeon2 gyro;
+    public ChassisSpeeds mSpeeds;
+    public SwerveDrivePoseEstimator robotPose;
+
+    // initializes widgets for field/odometry on shuffleboard & swerve angle
+    Field2d field = new Field2d();
+    ComplexWidget fieldShuffleboard = Shuffleboard.getTab("Field").add("2024-Field", field).withWidget(BuiltInWidgets.kField).withProperties(Map.of("robot icon size", 20));
+    GenericEntry swerveAngleEntry = Shuffleboard.getTab("Swerve").add("Swerve Angle", 0).getEntry();
+    GenericEntry encoderAngleEntry = Shuffleboard.getTab("Swerve").add("Encoder Angle",0).getEntry();
+
+
+    // Reporting values for AdvantageScope
+    StructArrayPublisher<SwerveModuleState> publisher = NetworkTableInstance.getDefault().getStructArrayTopic("MyStates", SwerveModuleState.struct).publish();
 
     public Swerve() {
         gyro = new Pigeon2(Constants.Swerve.pigeonID);
@@ -38,53 +57,105 @@ public class Swerve extends SubsystemBase {
         gyro.setYaw(0);
 
         mSwerveMods = new SwerveModule[] {
-                new SwerveModule(0, Constants.Swerve.Mod0.constants),
-                new SwerveModule(1, Constants.Swerve.Mod1.constants),
-                new SwerveModule(2, Constants.Swerve.Mod2.constants),
-                new SwerveModule(3, Constants.Swerve.Mod3.constants)
+            new SwerveModule(0, Constants.Swerve.Mod0.constants),
+            new SwerveModule(1, Constants.Swerve.Mod1.constants),
+            new SwerveModule(2, Constants.Swerve.Mod2.constants),
+            new SwerveModule(3, Constants.Swerve.Mod3.constants)
         };
 
         swerveOdometry = new SwerveDriveOdometry(Constants.Swerve.swerveKinematics, getGyroYaw(), getModulePositions());
+        robotPose = new SwerveDrivePoseEstimator(Constants.Swerve.swerveKinematics, getGyroYaw(), getModulePositions(), getPose());
+
+
+        // PathPlanner AutoBuilder
+
+        // AutoBuilder.configureHolonomic(
+        //     this::getPose, // Robot pose supplier
+        //     this::setPose, // Method to reset odometry (will be called if your auto has a starting pose)
+        //     this::getChassisSpeeds, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+        //     this::drive, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+        //     new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+        //             new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants TODO: update this
+        //             new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+        //             4.5, // Max module speed, in m/s TODO: update this
+        //             0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+        //             new ReplanningConfig() // Default path replanning config. See the API for the options here
+        //     ),
+        //     () -> {
+        //       // Boolean supplier that controls when the path will be mirrored for the red alliance
+        //       // This will flip the path being followed to the red side of the field.
+        //       // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+        //       var alliance = DriverStation.getAlliance();
+        //       if (alliance.isPresent()) {
+        //         return alliance.get() == DriverStation.Alliance.Red;
+        //       }
+        //       return false;
+        //     },
+        //     this // Reference to this subsystem to set requirements
+        // );
     }
 
+    // initialize pose estimator, needed for odometry autos
+
     public void drive(Translation2d translation, double rotation, boolean fieldRelative, boolean isOpenLoop) {
-        SwerveModuleState[] swerveModuleStates = Constants.Swerve.swerveKinematics.toSwerveModuleStates(
-                fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(
-                        translation.getX(),
-                        translation.getY(),
-                        rotation,
-                        getHeading())
-                        : new ChassisSpeeds(
-                                translation.getX(),
-                                translation.getY(),
-                                rotation));
+        SwerveModuleState[] swerveModuleStates =
+            Constants.Swerve.swerveKinematics.toSwerveModuleStates(
+                fieldRelative ? ChassisSpeeds.fromFieldRelativeSpeeds(  
+                                    translation.getX(), 
+                                    translation.getY(), 
+                                    rotation, 
+                                    getHeading()
+                                )
+                                : new ChassisSpeeds(
+                                    translation.getX(), 
+                                    translation.getY(), 
+                                    rotation)
+                                );
+        mSpeeds = new ChassisSpeeds(translation.getX(), translation.getY(), rotation);
         SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Constants.Swerve.maxSpeed);
 
-        for (SwerveModule mod : mSwerveMods) {
+        for(SwerveModule mod : mSwerveMods){
             mod.setDesiredState(swerveModuleStates[mod.moduleNumber], isOpenLoop);
         }
+    }
+
+    // Only robot relative drive mode used for pathplanner
+
+    public void drive(ChassisSpeeds speeds) {
+        SwerveModuleState[] swerveModuleStates = Constants.Swerve.swerveKinematics.toSwerveModuleStates(speeds);
+        SwerveDriveKinematics.desaturateWheelSpeeds(swerveModuleStates, Constants.Swerve.maxSpeed);
+        mSpeeds = speeds;
+        for(SwerveModule mod : mSwerveMods){
+            mod.setDesiredState(swerveModuleStates[mod.moduleNumber], true);
+        }
+    }
+
+    public ChassisSpeeds getChassisSpeeds() {
+        System.out.println(mSpeeds);
+        return mSpeeds;
     }
 
     /* Used by SwerveControllerCommand in Auto */
     public void setModuleStates(SwerveModuleState[] desiredStates) {
         SwerveDriveKinematics.desaturateWheelSpeeds(desiredStates, Constants.Swerve.maxSpeed);
-
-        for (SwerveModule mod : mSwerveMods) {
+        
+        for(SwerveModule mod : mSwerveMods){
             mod.setDesiredState(desiredStates[mod.moduleNumber], false);
         }
     }
 
-    public SwerveModuleState[] getModuleStates() {
+    public SwerveModuleState[] getModuleStates(){
         SwerveModuleState[] states = new SwerveModuleState[4];
-        for (SwerveModule mod : mSwerveMods) {
+        for(SwerveModule mod : mSwerveMods){
             states[mod.moduleNumber] = mod.getState();
         }
         return states;
     }
 
-    public SwerveModulePosition[] getModulePositions() {
+    public SwerveModulePosition[] getModulePositions(){
         SwerveModulePosition[] positions = new SwerveModulePosition[4];
-        for (SwerveModule mod : mSwerveMods) {
+        for(SwerveModule mod : mSwerveMods){
             positions[mod.moduleNumber] = mod.getPosition();
         }
         return positions;
@@ -98,26 +169,24 @@ public class Swerve extends SubsystemBase {
         swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), pose);
     }
 
-    public Rotation2d getHeading() {
+    public Rotation2d getHeading(){
         return getPose().getRotation();
     }
 
-    public void setHeading(Rotation2d heading) {
-        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(),
-                new Pose2d(getPose().getTranslation(), heading));
+    public void setHeading(Rotation2d heading){
+        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), new Pose2d(getPose().getTranslation(), heading));
     }
 
-    public void zeroHeading() {
-        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(),
-                new Pose2d(getPose().getTranslation(), new Rotation2d()));
+    public void zeroHeading (){
+        swerveOdometry.resetPosition(getGyroYaw(), getModulePositions(), new Pose2d(getPose().getTranslation(), new Rotation2d()));
     }
-
+   
     public Rotation2d getGyroYaw() {
         return Rotation2d.fromDegrees(gyro.getYaw().getValue());
     }
 
-    public void resetModulesToAbsolute() {
-        for (SwerveModule mod : mSwerveMods) {
+    public void resetModulesToAbsolute(){
+        for(SwerveModule mod : mSwerveMods){
             mod.resetToAbsolute();
         }
     }
