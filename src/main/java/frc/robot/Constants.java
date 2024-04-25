5a95b6abee86fe09ab641d6c383cf5a87837e72c
LamSauced <lnguyen0606@outlook.com>
2024-04-25 08:44:47 -0700

    DDR

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,205 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+// import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+import frc.lib.util.COTSTalonFXSwerveConstants;
+import frc.lib.util.SwerveModuleConstants;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+
+  public static final int motorID = 17;
+  public static final int jsID = 0;
+  public static final int intakeButtonID = 1;
+  public static final int outtakeButtonID = 12;
+  public static final int neoMotorID = 3;
+  public static final int sensorID = 1;
+  public static final int falconVelocityLimit = 0;
+  public static final int neoVelocityLimit = 0;
+
+  public static final int gearRatioArm = 151; 
+  public static final int gearRatioWrist = 72; 
+  public static final int leftmotorPort = 15;
+
+  public static final int rightmotorPort = 16;
+  public static final int wristMotorPort = 0; //Port for the motor
+  public static final int limitPort = 0; //Optical sensor that check if the arm is down
+   public static final int limitPortWrist = 3;
+  public static final int ENCODER_PORT = 2;
+
+  public static final double armLength = 22.5 * 0.0254;
+  public static final double wristLength = 11.4 * 0.0254;
+
+
+  public static final double AMP = -0.215;
+  public static final double REST = 0;
+  public static final double SPEAKER = -0.145 ;
+  public static final double FLOOR = 0;
+
+  
+  public static final double AMP_WRIST = 0.63;
+  public static final double REST_WRIST = 0;
+  public static final double SPEAKER_WRIST = 0;
+  public static final double FLOOR_WRIST = 0.47;
+  // public static final double FLOOR = 0.01;
+  // public static final double COUNT_PER_DEGREES = 0;
+
+
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+
+  public static final double stickRotationDeadband = 0.3;
+    public static final double stickDeadband = 0.05;
+    public static final int X_AXIS_PORT = 0;
+    public static final int Y_AXIS_PORT = 1;
+    public static final int ROTATIONAL_AXIS_PORT = 2;
+
+    public static final boolean FIELD_RELATIVE_MODE = true;
+
+    public static final class Swerve {
+        public static final double MAX_RADIANS_PER_SECOND = 12.773732;
+        public static final int pigeonID = 13;
+
+        public static final COTSTalonFXSwerveConstants chosenModule = COTSTalonFXSwerveConstants.SDS.MK4i.Falcon500(COTSTalonFXSwerveConstants.SDS.MK4i.driveRatios.L2);
+
+       
+        public static final double trackWidth = Units.inchesToMeters(30);
+        public static final double wheelBase = Units.inchesToMeters(30);
+        public static final double wheelCircumference = chosenModule.wheelCircumference;
+
+        
+         public static final SwerveDriveKinematics swerveKinematics = new SwerveDriveKinematics(
+            new Translation2d(wheelBase / 2.0, trackWidth / 2.0),
+            new Translation2d(wheelBase / 2.0, -trackWidth / 2.0),
+            new Translation2d(-wheelBase / 2.0, trackWidth / 2.0),
+            new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0));
+
+        /* Module Gear Ratios */
+        public static final double driveGearRatio = chosenModule.driveGearRatio;
+        public static final double angleGearRatio = chosenModule.angleGearRatio;
+
+        /* Motor Inverts */
+        public static final InvertedValue angleMotorInvert = chosenModule.angleMotorInvert;
+        public static final InvertedValue driveMotorInvert = chosenModule.driveMotorInvert;
+
+        /* Angle Encoder Invert */
+        public static final SensorDirectionValue cancoderInvert = chosenModule.cancoderInvert;
+
+        /* Swerve Current Limiting */
+        public static final int angleCurrentLimit = 25;
+        public static final int angleCurrentThreshold = 40;
+        public static final double angleCurrentThresholdTime = 0.1;
+        public static final boolean angleEnableCurrentLimit = true;
+
+        public static final int driveCurrentLimit = 35;
+        public static final int driveCurrentThreshold = 60;
+        public static final double driveCurrentThresholdTime = 0.1;
+        public static final boolean driveEnableCurrentLimit = true;
+
+        /* These values are used by the drive falcon to ramp in open loop and closed loop driving.
+         * We found a small open loop ramp (0.25) helps with tread wear, tipping, etc */
+        public static final double openLoopRamp = 0.25;
+        public static final double closedLoopRamp = 0.25;
+
+        /* Angle Motor PID Values */
+        public static final double angleKP = chosenModule.angleKP;
+        public static final double angleKI = chosenModule.angleKI;
+        public static final double angleKD = chosenModule.angleKD;
+
+        /* Drive Motor PID Values */
+        public static final double driveKP = 0.06;
+        public static final double driveKI = 0.0;
+        public static final double driveKD = 0.0;
+        public static final double driveKF = 0.0;
+
+        /* Drive Motor Characterization Values From SYSID */
+        public static final double driveKS = 0.32;
+        public static final double driveKV = 1.51;
+        public static final double driveKA = 0.21;
+
+        /* Swerve Profiling Values */
+        /** Meters per Second */
+        public static final double maxSpeed = 4.5;
+        /** Radians per Second */
+        public static final double maxAngularVelocity = 10.0;
+
+        /* Neutral Modes */
+        public static final NeutralModeValue angleNeutralMode = NeutralModeValue.Coast;
+        public static final NeutralModeValue driveNeutralMode = NeutralModeValue.Brake;
+
+        /* Module Specific Constants */
+        /* Front Left Module - Module 0 */
+        public static final class Mod0 {
+            public static final int driveMotorID = 2;
+            public static final int angleMotorID = 1;
+            public static final int canCoderID = 3;
+            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(86.4843755);
+            public static final SwerveModuleConstants constants = 
+                new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+        }
+
+        /* Front Right Module - Module 1 */
+        public static final class Mod1 {
+            public static final int driveMotorID = 4;
+            public static final int angleMotorID = 5;
+            public static final int canCoderID = 6;
+            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(114.9609375);
+            public static final SwerveModuleConstants constants = 
+                new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+        }
+        
+        /* Back Left Module - Module 2 */
+        public static final class Mod2 {
+            public static final int driveMotorID = 7;
+            public static final int angleMotorID = 8;
+            public static final int canCoderID = 9;
+            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(19.248046875);
+            public static final SwerveModuleConstants constants = 
+                new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+        }
+
+        /* Back Right Module - Module 3 */
+        public static final class Mod3 {
+            public static final int driveMotorID = 10;
+            public static final int angleMotorID = 11;
+            public static final int canCoderID = 12;
+            public static final Rotation2d angleOffset = Rotation2d.fromDegrees(-97.646484375);
+            public static final SwerveModuleConstants constants = 
+                new SwerveModuleConstants(driveMotorID, angleMotorID, canCoderID, angleOffset);
+        }
+    }
+
+    // public static final class AutoConstants {
+    //     public static final double kMaxSpeedMetersPerSecond = 3;
+    //     public static final double kMaxAccelerationMetersPerSecondSquared = 3;
+    //     public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
+    //     public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
+    
+    //     public static final double kPXController = 1;
+    //     public static final double kPYController = 1;
+    //     public static final double kPThetaController = 1;
+    
+    //     public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
+    //         new TrapezoidProfile.Constraints(
+    //             kMaxAngularSpeedRadiansPerSecond, kMaxAngularSpeedRadiansPerSecondSquared);
+    // }
+}
---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,166 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.ComplexWidget;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.commands.Arm.ArmMove;
+import frc.robot.commands.Autos.*;
+import frc.robot.commands.Arm.ArmStepDown;
+import frc.robot.commands.Arm.ArmStepUp;
+import frc.robot.commands.Grabber.Intake;
+import frc.robot.commands.Grabber.Outtake;
+import frc.robot.commands.Grabber.StopGrabber;
+import frc.robot.commands.Wrist.WristMove;
+import frc.robot.commands.Wrist.WristStepDown;
+import frc.robot.commands.Wrist.WristStepUp;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.Grabber;
+import frc.robot.subsystems.Swerve;
+import frc.robot.subsystems.Vision;
+import frc.robot.subsystems.Wrist;
+import frc.robot.subsystems.Arm.armPositionStates;
+import frc.robot.subsystems.Wrist.wristPositionStates;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+
+  public final static Grabber m_Grabber = new Grabber();
+
+  public static Joystick driver = new Joystick(1);
+  public static JoystickButton robotCentric = new JoystickButton(driver, 12);
+
+  public static Arm m_armLift = new Arm();
+  public static Wrist m_wrist = new Wrist();
+  public static Vision m_vision = new Vision();
+
+  // The robot's subsystems and commands are defined here...
+  // public static final Joystick test = new Joystick(0);
+  public static final Joystick operatorBoard = new Joystick(0);
+  // public static JoystickButton wristUp = new JoystickButton(operatorBoard, 5);
+  // public static JoystickButton wristDown = new JoystickButton(operatorBoard, 3);
+  public static JoystickButton floorIntake = new JoystickButton(operatorBoard, 2);
+  public static JoystickButton ampOuttake = new JoystickButton(operatorBoard, 4);
+  public static JoystickButton zeroGyro = new JoystickButton(operatorBoard, 8);
+
+
+  // public static JoystickButton armUp = new JoystickButton(operatorBoard, 4);
+  // public static JoystickButton armDown = new JoystickButton(operatorBoard, 2);
+  public static JoystickButton snipeOuttake = new JoystickButton(operatorBoard, 3);
+
+  // public JoystickButton intakeButton = new JoystickButton(operatorBoard, Constants.intakeButtonID);
+  public JoystickButton outtakeButton = new JoystickButton(operatorBoard, 5);
+  public static JoystickButton turnSwitch = new JoystickButton(operatorBoard, 105);
+  public static JoystickButton autoAngleButton = new JoystickButton(operatorBoard, 100);
+  public static JoystickButton boostButton = new JoystickButton(operatorBoard, 1);
+
+  public static Swerve s_Swerve = new Swerve();
+
+  // private final JoystickButton first = new JoystickButton(test, 1);
+  // private final JoystickButton second = new JoystickButton(test, 2);  
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  // private final CommandXboxController m_driverController =
+  //     new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final SendableChooser<Command> autoChooser = new SendableChooser<>();
+  ComplexWidget ShuffleBoardAutonomousRoutines = Shuffleboard.getTab("Driver").add("Autonomous Routines Selector", autoChooser).withWidget(BuiltInWidgets.kComboBoxChooser).withSize(2, 2).withPosition(0, 2);
+
+  // public final Command AmpRoutine = new AmpRoutine();
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    NamedCommands.registerCommand("Reset", new ResetArmAndWrist());
+    NamedCommands.registerCommand("Floor", new FloorPosRoutine());
+    NamedCommands.registerCommand("AmpWrist", new WristMove(wristPositionStates.SPEAKER));
+    NamedCommands.registerCommand("AmpArm", new ArmMove(armPositionStates.SPEAKER));
+    NamedCommands.registerCommand("Outtake", new Outtake().withTimeout(1));
+    NamedCommands.registerCommand("StopGrab", new StopGrabber().withTimeout(0.1));
+    NamedCommands.registerCommand("Intake", new Intake());
+    NamedCommands.registerCommand("Speaker", new SpeakerPosRoutine());
+    NamedCommands.registerCommand("Rest", new RestPosRoutine());
+    NamedCommands.registerCommand("Amp", new AmpPosRoutine());
+    NamedCommands.registerCommand("Snipe", new SnipePosRoutine().withTimeout(1.3));
+    //NamedCommands.registerCommand("Thing", new FloorRoutine());
+    // Configure the trigger bindings
+    configureBindings();
+    autoChooser.setDefaultOption("SPEAKER Routine", new SpeakerRoutine());
+    autoChooser.addOption("SidePathAway", new PathPlannerAuto("side"));
+    autoChooser.addOption("MiddlePath", new PathPlannerAuto("simpleAuto"));
+    autoChooser.addOption("guh", new PathPlannerAuto("guh"));
+    autoChooser.addOption("SidePathStage", new PathPlannerAuto("sideAutoStage"));
+    autoChooser.addOption("MiddlePathStage", new PathPlannerAuto("Copy of simpleAuto"));
+    autoChooser.addOption("Nothing", new PathPlannerAuto("nothing"));
+  }
+ 
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroHeading()));
+
+    floorIntake.onTrue(new ParallelCommandGroup(new FloorPosRoutine(), new Intake()));
+    ampOuttake.onTrue(new AmpPosRoutine());
+
+    snipeOuttake.onTrue(new SnipePosRoutine());
+
+    floorIntake.onFalse(new ParallelCommandGroup(new RestPosRoutine(), new StopGrabber()));
+    ampOuttake.onFalse(new RestPosRoutine());
+    snipeOuttake.onFalse(new RestPosRoutine());
+    //ArmStick.onTrue(new ArmMove(armPositionStates.SPEAKER));
+    // first.onTrue(new first());
+    // second.onTrue(new second());
+
+    // intakeButton.onTrue(new Intake());
+    outtakeButton.onTrue(new Outtake());
+    // snipeButton.onTrue(new SnipePosRoutine());
+
+    // intakeButton.onFalse(new StopGrabber());
+    // outtakeButton.onFalse(new StopGrabber());
+
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    // new Trigger(m_exampleSubsystem::exampleCondition)
+    //     .onTrue(new ExampleCommand(m_exampleSubsystem));
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+    // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * 
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+
+    // An example command will be run in autonomous
+    return autoChooser.getSelected();
+  }
+}
